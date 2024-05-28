1d7e19efd09275a23751691abed2ce0f5d6e7282
ericstober <erictstober@gmail.com>
2024-05-28 15:44:39 -0500

    added single meal details

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ function searchMeal(e){
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
     .then(res => res.json())
     .then(data => {
-      console.log(data);
       resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
 
       if (data.meals === null) {
@@ -29,7 +28,7 @@ function searchMeal(e){
         mealsElement.innerHTML = data.meals.map(meal => `
           <div class="meal">
             <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
-            <div class="meal-info" data-mealID="${meal.idMeal}">
+            <div class="meal-info" data-mealid="${meal.idMeal}">
               <h3>${meal.strMeal}</h3>
             </div>
           </div>
@@ -45,5 +44,62 @@ function searchMeal(e){
   }
 }
 
+// Get meal by ID
+function getMealByID(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+  .then(res => res.json())
+  .then(data => {
+    const meal = data.meals[0];
+
+    addMealToDOM(meal);
+  });
+}
+
+// Add meal to DOM
+function addMealToDOM(meal) {
+  const ingredients = [];
+
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+    } else {
+      break;
+    }
+  }
+
+  singleMealElement.innerHTML = `
+    <div class="single-meal">
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+      <div class="single-meal-info>
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+        ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+      </div>
+      <div class="main">
+        <p>${meal.strInstructions}</p>
+        <h2>Ingredients</h2>
+        <ul>
+          ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+        </ul>
+      </div>
+    </div>
+  `;
+}
+
 // Event listeners
 submit.addEventListener('submit', searchMeal);
+
+mealsElement.addEventListener('click', e => {
+  const mealInfo = e.composedPath().find(item => {
+    if (item.classList) {
+      return item.classList.contains('meal-info');
+    } else {
+      return false;
+    }
+  });
+
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute('data-mealid');
+    getMealByID(mealID);
+  }
+});
